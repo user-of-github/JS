d10d753f5f593ea10b051bfdafe6a6217aeb256b
Nikita <nikitosminsk100@gmail.com>
2025-03-03 16:49:51 +0300

    docs: package json

--- food-delivery-react-native/food-delivery-client/RootLayout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AppNavigation } from '@/navigation/Navigation';
-
-export const RootLayout: React.FC = () => {
-  return <AppNavigation />;
-};
---
+++ food-delivery-react-native/food-delivery-client/app/RootLayout.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { AppNavigation } from '@/navigation/Navigation';
+import type { NavigationScreensListType } from '@/navigation/types';
+import { useNavigationContainerRef } from '@react-navigation/native';
+import { useAuth } from '@/features/auth/AuthProvider';
+import { NavigationBar } from '@/components/layout/navigationBar';
+
+export const RootLayout: React.FC = () => {
+  const { user } = useAuth();
+  const [currentRoute, setCurrentRoute] = useState<string | null>(null);
+  const navigationRef = useNavigationContainerRef();
+
+  useEffect(() => {
+    setCurrentRoute(navigationRef.getCurrentRoute()?.name || null);
+
+    const changeRouteListener = navigationRef.addListener('state', () => {
+      setCurrentRoute(navigationRef.getCurrentRoute()?.name || null);
+    });
+
+    return () => {
+      navigationRef.removeListener('state', changeRouteListener);
+    };
+  }, []);
+
+  return (
+    <>
+      <AppNavigation navigationContainerRef={navigationRef as any} />
+      { user && currentRoute && (
+        <NavigationBar
+          navigate={navigationRef.navigate}
+          currentScreen={currentRoute as keyof NavigationScreensListType}
+        />
+      ) }
+    </>
+  );
+};
