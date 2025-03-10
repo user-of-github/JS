ac66061098411da467525d86ff643ef1f9801ed9
Nikita <nikitosminsk100@gmail.com>
2025-03-10 17:35:54 +0300

    docs

--- food-delivery-react-native/food-delivery-client/app/screens/explorer/index.tsx
+++ food-delivery-react-native/food-delivery-client/app/screens/explorer/index.tsx
@@ -11,7 +11,7 @@ export const ExplorerScreen: React.FC = () => {
 
   return (
     <NoScrollViewContainer className="h-full">
-      <View style={{ paddingVertical: AppLayoutPaddings.vertical, paddingBottom: AppLayoutPaddings.vertical * 2 }}>
+      <View style={{ paddingVertical: AppLayoutPaddings.vertical + AppLayoutPaddings.top, paddingBottom: AppLayoutPaddings.vertical * 2 }}>
         <View className="mb-10" style={{ paddingHorizontal: AppLayoutPaddings.horizontal }}>
           <Heading>Explorer</Heading>
         </View>
