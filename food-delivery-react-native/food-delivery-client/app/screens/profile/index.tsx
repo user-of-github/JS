e3984e2d3bbf515fb0c5f57f2e2962775259b741
Nikita <nikitosminsk100@gmail.com>
2025-03-14 20:43:38 +0300

    ran expo-optimize

--- food-delivery-react-native/food-delivery-client/app/screens/profile/index.tsx
+++ food-delivery-react-native/food-delivery-client/app/screens/profile/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { useAuthMutations } from '@/features/auth/useAuthMutations';
 import { useProfile } from '@/features/auth/useProfile';
 import { Container } from '@/components/layout/Container';
@@ -7,10 +7,16 @@ import { Button } from '@/components/ui/Button';
 import { Heading } from '@/components/ui/Heading';
 import { LoadingOverlay } from '@/components/ui/LoadingOverlay';
 import { LoadingSpinner } from '@/components/ui/LoadingSpinner';
+import { useAppNavigation } from '@/navigation/useAppNavigation';
+import { Feather } from '@expo/vector-icons';
+import { cn } from '@/components/utils';
+import { AppRoutes } from '@/navigation/routes';
 
 export const ProfileScreen: React.FC = () => {
   const { logout } = useAuthMutations();
   const { profile, isLoading } = useProfile();
+  const { navigate } = useAppNavigation();
+
 
   if (isLoading) {
     return (
@@ -49,12 +55,51 @@ export const ProfileScreen: React.FC = () => {
           </View>
         </View>
       </View>
-      <Button className="mt-10" textClassName="text-lg">
-        Log out
-      </Button>
-      <Button onPress={logout} className="mt-10" textClassName="text-lg">
-        Log out
-      </Button>
+      <View className="w-full flex-row gap-x-3 items-center mt-10">
+        <TouchableOpacity
+          activeOpacity={0.75}
+          onPress={() => navigate(AppRoutes.Cart.name)}
+          className="rounded-lg bg-secondary py-3 px-1 flex items-center justify-center w-[22%]"
+        >
+          <View className="flex flex-col items-center justify-center gap-x-3">
+            <Feather name="shopping-bag" size={22} color="#FFF" />
+            <Text className="text-white text-center text-lg">Cart</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.75}
+          className="rounded-lg bg-secondary py-3 px-1 flex items-center justify-center w-[22%]"
+          onPress={() => navigate(AppRoutes.Orders.name)}
+        >
+          <View className="flex flex-col items-center justify-center gap-x-3">
+            <Feather name="coffee" size={22} color="#FFF" />
+            <Text className="text-white text-center text-lg">Orders</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.75}
+          className="rounded-lg bg-secondary py-3 px-1 flex items-center justify-center w-[22%]"
+          onPress={() => navigate(AppRoutes.Favourites.name)}
+        >
+          <View className="flex flex-col items-center justify-center gap-x-3">
+            <Feather name="heart" size={22} color="#FFF" />
+            <Text className="text-white text-center text-lg">Favorites</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={logout}
+          activeOpacity={0.75}
+          className="rounded-lg bg-text-secondary py-3 px-1 flex items-center justify-center w-[22%]"
+        >
+          <View className="flex flex-col items-center justify-center gap-x-3">
+            <Feather name="log-out" size={22} color="#FFF" />
+            <Text className="text-white text-center text-lg">Log out</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </Container>
   );
 };
